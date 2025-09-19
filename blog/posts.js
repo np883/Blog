1ae74195ebd4d51bc1cc2c4bffc2cf2b49eb0e43
np883 <114385352+np883@users.noreply.github.com>
2025-09-19 08:58:32 +0100

    Update posts.js

--- blog/posts.js
+++ blog/posts.js
@@ -1,17 +1,18 @@
+// Array of all blog posts
 const posts = [
   {
     title: "My First Blog Post",
     file: "post1.html",
     date: "September 19, 2025",
-    preview: "This is a short preview of my first blog post. Click to read more..."
-    img: "https://via.placeholder.com/300x150"  // optional image
+    preview: "This is a short preview of my first blog post. Click to read more...",
+    img: "https://via.placeholder.com/300x150" // optional image
   },
   {
     title: "Learning Web Development",
     file: "post2.html",
     date: "September 20, 2025",
-    preview: "Today I learned how to structure a website using HTML, CSS, and JS..."
-    img: "https://via.placeholder.com/300x150"  // optional image
+    preview: "Today I learned how to structure a website using HTML, CSS, and JS...",
+    img: "https://via.placeholder.com/300x150"
   }
-  // Add new posts here
+  // Add new posts here easily
 ];
