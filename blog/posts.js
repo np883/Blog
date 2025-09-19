6283523fe5a4385a776a4c461d7dd739816982d3
np883 <114385352+np883@users.noreply.github.com>
2025-09-19 08:55:21 +0100

    Update posts.js

--- blog/posts.js
+++ blog/posts.js
@@ -4,12 +4,14 @@ const posts = [
     file: "post1.html",
     date: "September 19, 2025",
     preview: "This is a short preview of my first blog post. Click to read more..."
+    img: "https://via.placeholder.com/300x150"  // optional image
   },
   {
     title: "Learning Web Development",
     file: "post2.html",
     date: "September 20, 2025",
     preview: "Today I learned how to structure a website using HTML, CSS, and JS..."
+    img: "https://via.placeholder.com/300x150"  // optional image
   }
   // Add new posts here
 ];
