9bcc68740f86d70137a3ac053c5e5dbca1f82ae2
np883 <114385352+np883@users.noreply.github.com>
2025-09-19 08:30:10 +0100

    Create posts.js

---
+++ blog/posts.js
@@ -0,0 +1,17 @@
+// blog/posts.js
+
+const posts = [
+  {
+    title: "My First Blog Post",
+    file: "post1.html",
+    date: "September 19, 2025",
+    preview: "This is a short preview of my first blog post. Click to read more..."
+  },
+  {
+    title: "Learning Web Development",
+    file: "post2.html",
+    date: "September 20, 2025",
+    preview: "Today I learned how to structure a website using HTML, CSS, and JS..."
+  },
+  // Add new posts here easily
+];
